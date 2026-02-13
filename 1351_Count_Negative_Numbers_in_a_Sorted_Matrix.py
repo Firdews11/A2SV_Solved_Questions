59483be7a872794037d4e0966510f28b2340a7ec
Firdews11 <firdewshailu@gmail.com>
2026-02-13 16:48:23 +0300

    Create 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py

---
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count = 0
+        for i in grid:
+            left, right = 0, len(i)-1
+            while left <= right:
+                mid = (right +left)//2
+                if i[mid] < 0:
+                    right = mid -1
+                else: 
+                    left = mid +1
+            count += len(i) - left
+        return count
