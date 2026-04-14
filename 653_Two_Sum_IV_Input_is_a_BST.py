ee2a1a01924226f93f9eacb89292ca5e52a38dbd
Firdews11 <firdewshailu@gmail.com>
2026-04-14 19:34:07 +0300

    Create 653_Two_Sum_IV_Input_is_a_BST.py

---
+++ 653_Two_Sum_IV_Input_is_a_BST.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        
+        def inOrder(root):
+            if root:
+                inOrder(root.left)
+                ans.append(root.val)
+                inOrder(root.right)
+        ans = []
+        inOrder(root)
+        l, r = 0, len(ans)-1
+        while l< r:
+            if ans[l]+ans[r] > k:
+                r -=1
+            elif ans[l]+ans[r] < k:
+                l +=1
+            else: 
+                return True
+        return False
